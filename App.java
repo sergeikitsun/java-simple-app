e9378af0b4e17644c770d989677b3cba67bdfe91
sergeikitsun <sergei.kitsun201@gmail.com>
2022-09-06 20:20:17 +0300

    Revert "Update App.java"
    This reverts commit 15a39d1694f739e5abd64a90cf87a7c01945552c.

--- App.java
+++ App.java
@@ -9,6 +9,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+	Thread.sleep(2000);
+	System.out.println( "Hello World!" );
 	
     }
 }
